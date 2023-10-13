2c92f13c5bc35f922b4465ddd5bb02d7b2fc9ba7
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2023-10-13 11:34:38 +0200

    Delete 02/02-feladat directory

--- 02/02-feladat/js/script.js
+++
@@ -1,8 +0,0 @@
-var kiirashelye = document.getElementById("container2").innerHTML;
-
-function hozzaad(){
-    var a = document.getElementById("ujadat").value;
-    kiirashelye = `<div>${a}</div>`;
-}
-
-//kétszer ugyna az ne és ures mezo se
