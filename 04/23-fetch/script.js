1e78f792f8a2758c80fddb4d842e3625620fc927
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-03-07 22:57:44 +0100

    Add files via upload

---
+++ 04/23-fetch/script.js
@@ -0,0 +1,40 @@
+
+function FetchUser(){
+  let id = parseInt(document.getElementById("selectedId").value);
+
+  console.log(id);
+  fetch('https://jsonplaceholder.org/users/?id=1'/*+id*/)
+  .then(response => {
+    return response.json(); //1 utasitasnal nem kell return a then-nél, ha tobb van akkor return nélkül a következő nem kap adatot
+  })
+  .then(user => {
+    console.log(user);
+    consoleKi(user);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+
+
+// Id, kereszt és utónév, telefonszám, 
+//email, részletes cím adatok, geolokációs adatok,
+//weblap cím, cégadatok.
+
+function consoleKi(user){
+    console.log(user.id);
+    console.log(user.firstname);
+    console.log(user.lastname);
+    console.log(user.phone);
+    console.log(user.email);
+    console.log(user.address.zipcode);
+    console.log(user.address.city);
+    console.log(user.address.street);
+    console.log(user.address.suite);
+    console.log(user.address.geo.lat);
+    console.log(user.address.geo.lat);
+    console.log(user.website);
+    console.log(user.company.name);
+    console.log(user.company.catchPhrase);
+    console.log(user.company.bs);
+}
