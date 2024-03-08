838ca09aac0445c729e69d05ba7923ad0381eb13
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-03-08 09:13:14 +0100

    Add files via upload

---
+++ 04/_24-doga2/script.js
@@ -0,0 +1,65 @@
+L = (n , L0 , L1 , add) => {
+  let t = [L0,L1];
+  for(let i = 2; i < n; i++){
+      t[i] = t[i-2]+t[i-1]+add;
+  }
+  return t;
+}
+
+console.log(L(5, 1, 1, 1), [1, 1, 3, 5, 9]);
+console.log(L(5, 0, 0, 2), [0, 0, 2, 4, 8]);
+console.log(L(5, 0, 0, 0), [0, 0, 0, 0, 0]);
+console.log(L(10, 0, 1, 4), [0, 1, 5, 10, 19, 33, 56, 93, 153, 250]);
+
+
+console.log("Every nth");
+
+
+function every(arr, interval, start){
+  let t = [];
+  
+  for(let i = start || 0; i < arr.length; i += interval || 1)
+  {
+    t.push(arr[i]);
+  }
+  
+  return t;
+}
+
+console.log(every([0, 1, 2, 3, 4]), [0, 1, 2, 3, 4]);
+console.log(every([0, 1, 2, 3, 4], 1), [0, 1, 2, 3, 4]);
+console.log(every([0, 1, 2, 3, 4], 2), [0, 2, 4]);
+console.log(every([0, 1, 2, 3, 4], 3), [0, 3]);
+console.log(every([0, 1, 2, 3, 4], 3, 1), [1, 4]);
+
+console.log("Odd ones out")
+
+function oddOnesOut(nums) {
+  var t=[];
+
+  for (var i=0; i<nums.length; ++i)
+  {
+    var a=0;
+
+    for (var j=0; j<nums.length; ++j)
+    {
+      if (nums[i]==nums[j])
+      {
+        a++;
+      }
+    }
+
+    if (a%2==0){
+      t.push(nums[i]);
+    }
+      
+  }
+
+  return t;
+}
+
+console.log(oddOnesOut([1, 2, 3, 1, 3, 3]), [1, 1]);
+console.log(oddOnesOut([75, 68, 75, 47, 68]), [75, 68, 75, 68]);
+console.log(oddOnesOut([42, 72, 32, 4, 94, 82, 67, 67]), [67, 67]);
+console.log(oddOnesOut([100, 100, 5, 5, 100, 50, 68, 50, 68, 50, 68, 5, 100]), [100, 100, 100, 100]);
+console.log(oddOnesOut([82, 86, 71, 58, 44, 79, 50, 44, 79, 67, 82, 82, 55, 50]), [44, 79, 50, 44, 79, 50]);
