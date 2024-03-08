c4ded8d3a558f7c9e663b32ca2bb5fde46d17997
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-03-08 10:55:15 +0100

    Add files via upload

--- 04/23-fetch/script.js
+++ 04/23-fetch/script.js
@@ -1,15 +1,15 @@
-
 function FetchUser(){
   let id = parseInt(document.getElementById("selectedId").value);
 
   console.log(id);
-  fetch('https://jsonplaceholder.org/users/?id=1'/*+id*/)
+  fetch('https://jsonplaceholder.org/users/?id=1'+id)
   .then(response => {
     return response.json(); //1 utasitasnal nem kell return a then-nél, ha tobb van akkor return nélkül a következő nem kap adatot
   })
   .then(user => {
     console.log(user);
     consoleKi(user);
+    KiIr(user);
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
@@ -32,9 +32,28 @@ function consoleKi(user){
     console.log(user.address.street);
     console.log(user.address.suite);
     console.log(user.address.geo.lat);
-    console.log(user.address.geo.lat);
+    console.log(user.address.geo.lng);
     console.log(user.website);
     console.log(user.company.name);
     console.log(user.company.catchPhrase);
     console.log(user.company.bs);
+}
+
+function KiIr(user){
+  document.getElementById("ki").innerHTML = "";
+  document.getElementById("ki").innerHTML += user.id +"<br>" 
+  +user.firstname +"<br>" 
+  +user.lastname +"<br>" 
+  +user.phone +"<br>" 
+  +user.email +"<br>" 
+  +user.address.zipcode +"<br>" 
+  +user.address.city +"<br>" 
+  +user.address.street+"<br>" 
+  +user.address.suite +"<br>" 
+  +user.address.geo.lat +"<br>" 
+  +user.address.geo.lng +"<br>" 
+  +user.website +"<br>" 
+  +user.company.name +"<br>"
+  +user.company.catchPhrase +"<br>"
+  +user.company.bs +"<br>"
 }
