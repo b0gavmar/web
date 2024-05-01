ffde22c1948899b04accb9fab6fbb13a2b090f32
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-05-02 00:27:44 +0200

    Add files via upload

--- 03-projekt/js/script.js
+++ 03-projekt/js/script.js
@@ -2,6 +2,8 @@ var kNev = "";
 var kId;
 var dNev = "";
 var dId;
+var kurzusokSzama;
+var diakokSzama;
 
 /*
 document.getElementById("kid").addEventListener("input", function() {
@@ -18,7 +20,38 @@ document.getElementById("dnev").addEventListener("input", event => {
 });
 */
 
-function Load(){
+function Basic(){
+    document.getElementById("kid").addEventListener("input", function() {
+        kId = parseInt(document.getElementById("kid").value);
+        kNev = NaN;
+        document.getElementById("knev").value = "";
+    });
+    
+    document.getElementById("knev").addEventListener("input", function() {
+        kNev = document.getElementById("knev").value;
+        kId = NaN;
+        document.getElementById("kid").value ="";
+    });
+    
+    document.getElementById("dnev").addEventListener("input", function() {
+        dNev = document.getElementById("dnev").value;
+        dId =NaN;
+        document.getElementById("did").value ="";
+    });
+
+    document.getElementById("did").addEventListener("input", function() {
+        dId = parseInt(document.getElementById("did").value);
+        dNev =NaN;
+        document.getElementById("dnev").value ="";
+    });
+
+    document.getElementById("kid").value = parseInt();
+    document.getElementById("knev").value = "";
+    document.getElementById("dnev").value = "";
+    document.getElementById("did").value = parseInt();
+}
+
+function LoadCourses(){
     fetch("https://vvri.pythonanywhere.com/api/courses")
     .then(response => response.json())
     .then(json => {
@@ -31,27 +64,6 @@ function Load(){
             </div>`;
         });
         document.getElementById("courses").innerHTML = li;
-    }).then(function(){
-        document.getElementById("kid").addEventListener("input", function() {
-            kId = parseInt(document.getElementById("kid").value);
-        });
-        
-        document.getElementById("knev").addEventListener("input", function() {
-            kNev = document.getElementById("knev").value;
-        });
-        
-        document.getElementById("dnev").addEventListener("input", function() {
-            dNev = document.getElementById("dnev").value;
-        });
-
-        document.getElementById("did").addEventListener("input", function() {
-            dId = parseInt(document.getElementById("did").value);
-        });
-
-        document.getElementById("kid").value = parseInt();
-        document.getElementById("knev").value = "";
-        document.getElementById("dnev").value = "";
-        document.getElementById("did").value = parseInt();
     }).catch(error => console.error('Hiba:', error));
 }
 
@@ -64,22 +76,20 @@ function AddCourse(){
     }
     else{
         fetch("https://vvri.pythonanywhere.com/api/courses", {
-            // Metódus hozzáadása
+
             method: "POST",
               
-            // Küldendő test vagy tartalom hozzáadása
             body: JSON.stringify({
                 name: kNev,
             }),
              
-            // Fejlécek hozzáadása a kéréshez
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
             }
-        }).then(a => {
+        }).then(() => {
             console.log("Kurzus sikeresen hozzáadva");
-            Load();
-        }).catch(()=>(console.log("Hiba kurzus hozzáadásakor")));
+            LoadCourses();
+        }).catch(error=>(console.log("Hiba kurzus hozzáadásakor: "+ error)));
     }
     
 }
@@ -88,7 +98,29 @@ function AddCourse(){
 function SearchCourse(){
     //console.log(kId);
     if(Number.isNaN(kId) == true){
-        Load();
+        fetch(`https://vvri.pythonanywhere.com/api/courses`)
+        .then(response => response.json())
+        .then(courses => {
+            let li = `<div>`;
+                courses.forEach(course =>{
+                if(course.name == kNev){
+                    li+=`${course.id}. ${course.name} 
+                    <br><br>
+                    Diákok:
+                    <list>
+                        <ul>
+                    `
+                    course.students.forEach(student =>{
+                        li+=`<li>id:${student.id} - név:${student.name} </li>`;
+                    });
+                    li+=`</ul></list></div>`;
+                }
+            });
+            
+            li+=`</ul></list></div>`;
+            document.getElementById("courses").innerHTML = li;
+                    
+        }).catch(() => (console.log("Hiba kurzus kereséskor")));
     }
     else{
         fetch(`https://vvri.pythonanywhere.com/api/courses/${kId}`)
@@ -120,43 +152,60 @@ function AddStudent(){
     }else{
         fetch(`https://vvri.pythonanywhere.com/api/students`, {
      
-        // Metódus hozzáadása
         method: "POST",
          
-        // Küldendő test vagy tartalom hozzáadása
         body: JSON.stringify({
             name: dNev,
             course_id: kId,
         }),
          
-        // Fejlécek hozzáadása a kéréshez
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
         }).then(response => response.json())
-        .then(diak =>{
+        .then(() =>{
             console.log("Diák hozzáadva")
         }).catch(error => (console.log("Hiba diák hozzáadásakor:"+error)));
     }
     
 }
 
+function TorolDiakID(){
+    let tor;
+    return tor;
+}
+
 function RemoveStudent(){
     //console.log(dId);
+    let torolniAkartDiakId;
     if(Number.isNaN(dId)){
-
+        
+        fetch(`https://vvri.pythonanywhere.com/api/students`)
+        .then(response => response.json())
+        .then(students => {
+            students.forEach(student => {
+                if(student.name == dNev){
+                    fetch(`https://vvri.pythonanywhere.com/api/students/${student.id}`, {
+     
+                        method: "DELETE",
+        
+                    })
+                }
+            });
+        })
+        .then(() =>{
+            console.log("Diák törölve")
+        });
     }
     else{
         fetch(`https://vvri.pythonanywhere.com/api/students/${dId}`, {
      
-        // Metódus hozzáadása
         method: "DELETE",
         
         }).then(response => response.json())
         .then(() =>{
             console.log("Diák törölve")
-        })
-        .then(() => (SearchCourse()))
+        });
         /*.catch(error => (console.log("Hiba diák törlésekor: "+error)))*/;
     }
 }
@@ -164,18 +213,32 @@ function RemoveStudent(){
 
 //hha túl nagy az id, az nem megy
 function SearchStudent(){
-    console.log(`https://vvri.pythonanywhere.com/api/students/${dId}`);
-    if(Number.isNaN(dId)){
+    if(Number.isNaN(dId) == true){
+        fetch(`https://vvri.pythonanywhere.com/api/students`)
+        .then(response => response.json())
+        .then(students => {
+            let li;
+            students.forEach(student => {
+                if(student.name == dNev){
+                    li = `<div>
+                            ${student.id}. ${student.name} 
+                            <br><br>
+                            Adatok változtatása:
 
-    }else{
+                            
+                            </div>`;
+                }
+            })
+
+            
+                document.getElementById("courses").innerHTML = li;
+                
+                    
+        }).catch(error => console.log("Hiba diák keresésekor:"+error));
+    }
+    else{
         fetch(`https://vvri.pythonanywhere.com/api/students/${dId}`)
         .then(response => response.json())
-        /*.then(response => {
-            if (!response.ok) {
-                throw new Error("A diák nincs.");
-            }
-            return response.json();
-        })*/
         .then(student => {
             let li = `<div>
                             ${student.id}. ${student.name} 
