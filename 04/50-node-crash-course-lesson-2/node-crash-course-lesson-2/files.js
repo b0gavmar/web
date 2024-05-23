da6639c2f02d59c56bcb802537af49cd6fbe2544
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-05-23 15:16:59 +0200

    Add files via upload

---
+++ 04/50-node-crash-course-lesson-2/node-crash-course-lesson-2/files.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.version());
+console.log(os.homedir());
+console.log(os.totalmem());
+console.log(os.freemem());
+
+// reading files
+fs.readFile('./docs/blog.txt', (err, data) => {
+  if (err) {
+    console.log(err);
+  }  
+  console.log(data.toString());
+});
+
+// console.log('last line');
+
+// writing files
+// fs.writeFile('./docs/blog.txt', 'hello, world', () => {
+//   console.log('file was written');
+// });
+
+fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
+  console.log('file was written');
+});
+
+// directories
+if (!fs.existsSync('./assets')) {
+  fs.mkdir('./assets', err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('folder created');
+  });
+} else {
+  fs.rmdir('./assets', err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('folder deleted');
+  });
+}
+
+// deleting files
+if (fs.existsSync('./docs/deleteme.txt')) {
+  fs.unlink('./docs/deleteme.txt', err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('file deleted');
+  });
+}
